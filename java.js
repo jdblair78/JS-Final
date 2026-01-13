1c9c86820a3a2cf8bfc6b96c0b58e826d4f3b06a
jdblair78 <jdblair78@gmail.com>
2026-01-13 17:02:49 -0500

    final

--- java.js
+++ java.js
@@ -1,1 +1,12 @@
+const API_KEY = "97fda536b4a1f3d474285cfc3cabf0a29a0f3e09";
 
+const url = `https://api.example.com/comics?apikey=${API_KEY}`;
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
+
+
+  
