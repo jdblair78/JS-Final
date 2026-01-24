7ce39277899ea24c5a3899ae16f153dd98b48505
jdblair78 <jdblair78@gmail.com>
2026-01-24 08:58:49 -0500

    final

--- java.js
+++ java.js
@@ -4,14 +4,18 @@
 
 const searchInput = document.getElementById("search");
 const resultsList = document.getElementById("results");
+const filterSelect = document.getElementById("filter")
 
 let debounceTimer;
+let currentMovies = []
 
 searchInput.addEventListener("input", () => {
   clearTimeout(debounceTimer);
 
   debounceTimer = setTimeout(() => {
     const query = searchInput.value.trim();
+
+    console.log("USER TYPED:", query)
 
     if (query.length < 3) {
       resultsList.innerHTML = "";
@@ -26,18 +30,32 @@ async function fetchMovies(searchTerm) {
   const apiKey = "ab612e22";
   const url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
 
+  console.log("API URL:", url)
+
   try {
     const response = await fetch(url);
     const data = await response.json();
     const max_results = 6
+    
+     console.log("FULL API RESPONSE:", data)
+
+    
 
     if (data.Response === "True") {
-      displayResults(data.Search.slice(0,max_results));
-    } else {
+
+      currentMovies = data.Search.slice(0, max_results);
+        displayResults(currentMovies);
+
+      
+    } 
+    
+    else {
       resultsList.innerHTML = "<li>No results found</li>";
     }
-  } catch (error) {
-    console.error("Error fetching data:", error);
+  } 
+  
+  catch (error) {
+    console.error("FETCH ERROR:", error);
   }
 }
 
@@ -45,6 +63,9 @@ function displayResults(movies) {
   results.innerHTML = "";
 
   movies.forEach(movie => {
+
+    console.log("MOVIE OBJECT:", movie)
+
     const movieCard = document.createElement("div");
     movieCard.className = "movie-card";
 
@@ -62,8 +83,40 @@ function displayResults(movies) {
     const year = document.createElement("p");
     year.textContent = movie.Year;
 
+    
+    
     movieCard.appendChild(poster);
-
-    results.appendChild(movieCard);
+    movieCard.appendChild(title)
+    movieCard.appendChild(year)
+    
+    resultsList.appendChild(movieCard);
   });
 }
+
+filterSelect.addEventListener("change", () => {
+  if (currentMovies.length === 0)  
+    return    
+  
+  const filter = filterSelect.value
+  let sortedMovies = [...currentMovies]
+    
+
+
+    if (filter === 'A_TO_Z') {
+      sortedMovies.sort((a, b) => (a.Title.localeCompare(b.Title)))
+    }
+
+    if (filter === 'Z_TO_A') {
+      sortedMovies.sort((a, b) => (b.Title.localeCompare(a.Title)) )
+    }
+
+    if (filter === 'RELEASE_ASC') {
+      sortedMovies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year))
+    }
+    if (filter === 'RELEASE_DSC') {
+      sortedMovies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year))
+    }
+
+    displayResults(sortedMovies)
+  })
+
